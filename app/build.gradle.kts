0da9ab8aeafc65a9871b60c241b36a19eaf0f02c
Maxim <m.myheev@gmail.com>
2025-10-28 20:05:11 +0300

    feat: update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,7 +35,6 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("com.google.android.material:material:1.13.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
@@ -45,6 +44,6 @@ dependencies {
     implementation("androidx.navigation:navigation-ui:2.9.5")
 
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit:1.3.0")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
 }
